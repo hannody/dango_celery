36fd3c66a7d1a5fb32c55deed99ccc3fa554dbbf
Mohanad Abu-Nayla <aljaboori@gmail.com>
2021-10-14 17:27:09 +0800

    Update: Dockerizing and some templates, a form and views.

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # We are adding in the directory where our templates will be stored.
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
